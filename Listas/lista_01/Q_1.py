453a67c7812047afbed5a4502b4fa301e7def35b
João Victor Bezerra Gomes <jvbg16@gmail.com>
2025-04-18 02:45:44 -0300

    Update Q_1.py

--- Listas/lista_01/Q_1.py
+++ Listas/lista_01/Q_1.py
@@ -2,4 +2,9 @@ nome = "Joao Victor"
 cidade = "Natal"
 idade = 19
 print("Ola, sou " + nome + "." + " Moro em " + cidade + " e tenho " + str(idade) + " anos.")
- 
+if ( x >= 12):
+  print(successful)
+else:
+  print(faild)
+  
+
